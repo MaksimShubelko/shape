ece705b3455baaaf002d418101c9d21a141c50d1
shube <shubelkom@gmail.com>
2021-11-25 16:35:58 +0300

    Fix tests

--- src/test/java/by/test/shubelko/ballServiceImpl/BallServiceImplTest.java
+++ src/test/java/by/test/shubelko/ballServiceImpl/BallServiceImplTest.java
@@ -2,7 +2,6 @@ package by.test.shubelko.ballServiceImpl;
 
 import by.task.shubelko.creator.BallCreator;
 import by.task.shubelko.entity.Ball;
-import by.task.shubelko.entity.Point;
 import by.task.shubelko.exception.BallException;
 import by.task.shubelko.service.ShapeService;
 import by.task.shubelko.service.impl.BallServiceImpl;
@@ -11,16 +10,16 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class BallServiceImplTest {
-    private static final double coordinateX = 1;
-    private static final double coordinateY = 1;
-    private static final double coordinateZ = 1;
+    private static final double coordinateX = 4;
+    private static final double coordinateY = 4;
+    private static final double coordinateZ = 4;
     private static final double radius = 4;
 
     @Test
     public void testCalculateSurfaceArea() throws BallException {
         ShapeService calculation = new BallServiceImpl();
         BallCreator ballCreator = new BallCreator();
-        Ball ball = ballCreator.createBall(1, 2, 3, 4);
+        Ball ball = ballCreator.createBall(coordinateX, coordinateY, coordinateZ, radius);
         double actualSurfaceArea = calculation.calculateSurfaceArea(ball);
         double expectedSurfaceArea = 64 * Math.PI;
         assertEquals(actualSurfaceArea, expectedSurfaceArea, 0.001, "Test failed as...");
@@ -30,7 +29,7 @@ public class BallServiceImplTest {
     public void testCalculateBallVolume() throws BallException {
         ShapeService calculation = new BallServiceImpl();
         BallCreator ballCreator = new BallCreator();
-        Ball ball = ballCreator.createBall(1, 2, 3, 4);
+        Ball ball = ballCreator.createBall(coordinateX, coordinateY, coordinateZ, radius);
         double actualVolume = calculation.calculateBallVolume(ball);
         double expectedVolume = 256 * Math.PI / 3;
         assertEquals(actualVolume, expectedVolume, 0.001, "Test failed as...");
@@ -40,7 +39,7 @@ public class BallServiceImplTest {
     public void testCalculatePartVolumeByPlaneParallelXY() throws BallException {
         ShapeService calculation = new BallServiceImpl();
         BallCreator ballCreator = new BallCreator();
-        Ball ball = ballCreator.createBall(1, 2, 4, 4);
+        Ball ball = ballCreator.createBall(coordinateX, coordinateY, coordinateZ, radius);
         double actualVolume = calculation.calculatePartVolumeByPlaneParallelXY(ball, 2);
         double expectedVolume = 1;
         assertEquals(actualVolume, expectedVolume, 0.001, "Test failed as...");
@@ -50,7 +49,7 @@ public class BallServiceImplTest {
     public void testCalculatePartVolumeByPlaneParallelXZ() throws BallException {
         ShapeService calculation = new BallServiceImpl();
         BallCreator ballCreator = new BallCreator();
-        Ball ball = ballCreator.createBall(1, 4, 4, 4);
+        Ball ball = ballCreator.createBall(coordinateX, coordinateY, coordinateZ, radius);
         double actualVolume = calculation.calculatePartVolumeByPlaneParallelXZ(ball, 2);
         double expectedVolume = 1;
         assertEquals(actualVolume, expectedVolume, 0.001, "Test failed as...");
@@ -70,7 +69,7 @@ public class BallServiceImplTest {
     public void testIsBall() throws BallException {
         ShapeService calculation = new BallServiceImpl();
         BallCreator ballCreator = new BallCreator();
-        Ball ball = ballCreator.createBall(1, 1, 1, 0);
+        Ball ball = ballCreator.createBall(coordinateX, coordinateY, coordinateZ, 0);
         boolean actual = calculation.isBall(ball);
         assertFalse(actual, " Test failed as...");
     }
@@ -79,7 +78,7 @@ public class BallServiceImplTest {
     public void testIsPlaneSwing() throws BallException {
         ShapeService calculation = new BallServiceImpl();
         BallCreator ballCreator = new BallCreator();
-        Ball ball = ballCreator.createBall(1, 5, 1, 0);
+        Ball ball = ballCreator.createBall(coordinateX, coordinateY, coordinateZ, radius);
         boolean actual = calculation.isPlaneSwing(ball);
         assertFalse(actual, " Test failed as...");
     }
